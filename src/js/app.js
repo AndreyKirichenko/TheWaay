830305f837a67503970b4e2abcfbe098cecf1f01
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-10-15 14:12:26 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,10 @@ import '../pug/index.pug'
 
 import Navigation from './menu'
 import Form from './form'
+import Slider from './slider'
 
 document.addEventListener("DOMContentLoaded", function(event) {
   Navigation();
   Form();
+  Slider();
 });
