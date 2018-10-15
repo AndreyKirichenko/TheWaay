e31e74b5d573b5f9c19852b9507c7a1e5e584d8a
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-10-15 14:11:34 +0300

    upd

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+// import {tns} from 'tiny-slider/src/tiny-slider.module.js';
+
+
+function slider() {
+  // let slider = tns({
+  //   container: '.my-slider',
+  //   items: 1
+  // });
+}
+
+export default slider();
