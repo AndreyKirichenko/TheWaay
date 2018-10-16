11fd75c64ec22932d6926aa35872df6c42b37b5a
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-10-16 18:01:32 +0300

    updated slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,10 +2,26 @@ import Swiper from 'swiper';
 
 
 function slider() {
-  let swiper = new Swiper('.swiper-container', {
+  let mySwiper = initSlider();
+
+  window.addEventListener('resize', function(){
+    mySwiper.destroy();
+    mySwiper = undefined;
+    mySwiper = initSlider();
+  });
+}
+
+function initSlider() {
+  return new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 7000,
+    },
+    autoResize: false,
+    loop: true,
+    resizeReInit: true,
     navigation: {
-      nextEl: '.swiper__next',
-      prevEl: '.swiper__prev',
+      nextEl: '.weHelpSlider__button--next',
+      prevEl: '.weHelpSlider__button--prev',
     },
   });
 }
