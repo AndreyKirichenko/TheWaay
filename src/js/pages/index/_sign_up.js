c84020f70412a8fa1bd5aef35dca4a6b1340c4ca
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-10-29 02:18:43 +0300

    added buttonto sign in page

--- src/js/pages/index/_sign_up.js
+++ src/js/pages/index/_sign_up.js
@@ -4,7 +4,7 @@ import IMask from "imask";
 
 let home;
 
-function signUp() {
+function signUp() { 
   new IMask(document.querySelector('.textInput--tel'), { mask: '+{7}(000)000-00-00'});
   home = document.querySelector('.signUp');
   blurOnFocus();
